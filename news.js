e6723752abfd5192d28e1a8802d763661f0552b6
achu62 <b.c.s.udaar@gmail.com>
2026-07-23 21:32:18 +0530

    18

--- news.js
+++ news.js
@@ -1,1 +1,8 @@
-export const news = [{heading:"v1.7.0 is released"}];
+export const news = [
+    {heading:`
+        Noferic-IDE-1.7.5 is released            on:23-7-2026(Day-Month-Year)`
+    ,
+        content:`Noferic-IDE-1.7.5 is released\n Its integrated with  typescript-language-server for better folder-wide understanding \n Note:now  only the auto complete is implemented and  it is also  experimental
+
+`},
+    {heading:"v1.7.0 is released"}];
